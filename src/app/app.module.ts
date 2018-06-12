1a6b4186abd803ad970395384c3fd0c1263758e2
Bobby <bobby@msn.com>
2018-06-13 00:21:44 +0200

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {StocksService} from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutes} from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutes
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
