5bd4b91d9046d6891540c443a3c8efe962f26a0d
Bobby <bobby@msn.com>
2018-06-12 22:55:07 +0200

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import {StocksService} from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
